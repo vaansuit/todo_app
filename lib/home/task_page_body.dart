c84b74fde86cf3afd4c50f7b1fd5309b87509b0a
Victor Hugo Vansuit <Victor.Vansuit@proway.treina>
2022-07-13 16:58:40 -0300

    [style] adjusted carrousel, now has an effect ~^.^~

--- lib/home/task_page_body.dart
+++ lib/home/task_page_body.dart
@@ -12,17 +12,21 @@ class TaskPageBody extends StatefulWidget {
 
 class _TaskPageBodyState extends State<TaskPageBody> {
   PageController pageController = PageController(viewportFraction: 0.85);
-  var _currentPageValue = 0.0;
+  var _currPageValue = 0.0;
   double _scaleFactor = 0.8;
 
   @override
   void initState() {
     super.initState();
-    pageController.addListener(() {
-      setState(() {
-        _currentPageValue = pageController.page!;
-      });
-    });
+    pageController.addListener(
+      () {
+        setState(
+          () {
+            _currPageValue = pageController.page!;
+          },
+        );
+      },
+    );
   }
 
   //evitando leak memory
@@ -44,93 +48,101 @@ class _TaskPageBodyState extends State<TaskPageBody> {
           }),
     );
   }
-}
 
-Widget _buildTaskPageBody(int index) {
-  return Container(
-    height: 120,
-    margin: const EdgeInsets.only(
-      left: 10,
-      right: 10,
-    ),
-    decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(30), color: Colors.grey[400]),
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Container(
-          height: 150,
-          width: 250,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(30),
-            boxShadow: [
-              BoxShadow(
-                color: AppColors.shadowColors,
-                spreadRadius: 1.5,
-              ),
-            ],
-          ),
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(
-                  top: 3,
-                  bottom: 5,
-                  left: 15,
-                  right: 15,
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    IconButton(
-                      onPressed: () {},
-                      icon: Icon(Icons.event),
-                    ),
-                    IconButton(
-                      onPressed: () {},
-                      icon: Icon(Icons.close),
-                    ),
-                  ],
-                ),
-              ),
-              Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 5),
-                    child: Row(
-                      children: [
-                        SimpleText(
-                          text: 'Academia',
-                          color: Colors.black,
-                        ),
-                      ],
-                    ),
+  Widget _buildTaskPageBody(int index) {
+    Matrix4 matrix = new Matrix4.identity();
+    if (index == _currPageValue.floor()) {
+      var currScale = 1 - (_currPageValue - index) * (1 - _scaleFactor);
+      matrix = Matrix4.diagonal3Values(1, currScale, 1);
+    } else if (index == _currPageValue.floor() + 1) {
+      var currScale =
+          _scaleFactor + (_currPageValue - index + 1) * (1 - _scaleFactor);
+      matrix = Matrix4.diagonal3Values(1, currScale, 1);
+    }
+
+    return Transform(
+      transform: matrix,
+      child: Container(
+        height: 120,
+        margin: const EdgeInsets.only(
+          left: 10,
+          right: 10,
+        ),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30), color: Colors.grey[400]),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 150,
+              width: 250,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(30),
+                boxShadow: [
+                  BoxShadow(
+                    color: AppColors.shadowColors,
+                    spreadRadius: 1.5,
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 5),
-                    child: Row(
-                      children: [
-                        SimpleText(
-                          text: 'Horário: 17:35',
-                          color: Colors.black,
-                          size: 16,
-                        ),
-                      ],
-                    ),
-                  ),
-                  // Container(
-                  //   height: 90,
-                  //   width: 200,
-                  //   color: Colors.black,
-                  // ),
                 ],
               ),
-            ],
-          ),
-        ),
-      ],
-    ), //ternario (if\else)
-  );
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      top: 3,
+                      bottom: 5,
+                      left: 15,
+                      right: 15,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(Icons.event),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(Icons.close),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 5),
+                        child: Row(
+                          children: [
+                            SimpleText(
+                              text: 'Academia',
+                              color: Colors.black,
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 5),
+                        child: Row(
+                          children: [
+                            SimpleText(
+                              text: 'Horário: 17:35',
+                              color: Colors.black,
+                              size: 16,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ), //ternario (if\else)
+      ),
+    );
+  }
 }
